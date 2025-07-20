ccb3c1b768c1cfd77b53adb105f60508f135a80b
YordanosT1 <jordan.esh88@gmail.com>
2025-07-20 06:49:30 +0300

    forgotten

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
+from .views import list_books, LibraryDetailView
+
 
 urlpatterns = [
     # Existing ones...
